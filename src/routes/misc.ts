12959af9b79ba9a7bb05c583b18290d09f8a8c85
Hitesh Dutt <er.hiteshdutt@gmail.com>
2025-01-03 08:50:16 +0530

    check user id

---
+++ src/routes/misc.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { misc } from "@/controllers";
+import requestValidator from "@/middlewares/request-validator";
+import sessionValidator from "@/middlewares/session-validator";
+import { query } from "express-validator";
+
+const router = Router({ mergeParams: true });
+router.get(
+    "/exists/:jid",
+    query("jid").isString(),
+    requestValidator,
+    sessionValidator,
+    misc.exists,
+);
+
+export default router;
